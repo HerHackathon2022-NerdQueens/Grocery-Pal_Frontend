658ba7df14684bded771b41991599b15f2d86fdc
sara alawad <sara4sy@gmail.com>
2022-06-24 16:28:55 +0200

    css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,15 @@
+import './footer.css'
+import { BsShopWindow } from "react-icons/bs";
+
 export default function Footer() {
     return (
-        <nav>
+        <nav className="navigation">
             <ul>
-                <li>Random Recipes</li>
+                <li>
+                    {/* Random Recipes */}
+
+                    <BsShopWindow />
+                </li>
                 <li>User List</li>
                 <li>Premade Lists</li>
                 <li>Statistics</li>
