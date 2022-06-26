62f1be4faa9903b4fd8a38f2e750ebce5951aa33
sara alawad <sara4sy@gmail.com>
2022-06-26 03:35:29 +0200

    fixing css bugs

--- src/Components/AddMultipleItemsToShoppingList.jsx
+++ src/Components/AddMultipleItemsToShoppingList.jsx
@@ -19,7 +19,7 @@ export default function AddMultipleItemsToShoppingList(props) {
 
     return (
         <>
-            <button onClick={e => addItem(e, props.id, props.amount)}>+ add mult</button>
+            <button onClick={e => addItem(e, props.id, props.amount)} className="plus-btn btn">+</button>
         </>
     )
 
